603cab24d1d7c3bdb052a8b226bd259ee44643bb
Fatma R <anthoschichequs@gmail.com>
2025-08-22 10:54:34 +0300

    degisiklikler yapildi/

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
+        "./app/Filament/**/*.php",
+        "./resources/views/filament/**/*.blade.php",
+        "./vendor/filament/**/*.blade.php",
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
